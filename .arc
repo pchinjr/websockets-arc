83dc964764518c9ddeca24370387b40e20961049
Paul <pchinjr@gmail.com>
2019-07-08 13:04:57 -0400

    redo hello world

--- .arc
+++ .arc
@@ -1,15 +1,9 @@
 @app 
-ws-apigw
+ws-demo
 
 @aws
 region us-east-1
 profile stepfunctions-user
-
-@ws
-
-@static
-staging pchinjr-ws-staging
-production pchinjr-ws-production
 
 @http
 get /
--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,24 +1,18 @@
 
-let arc = require('@architect/functions');
-let getURL = require('./get-web-socket-url');
-let static = assetPath => `${process.env.NODE_ENV}/_static/${assetPath}`;
+// let arc = require('@architect/functions');
+// let getURL = require('./get-web-socket-url');
+// let static = assetPath => `${process.env.NODE_ENV}/_static/${assetPath}`;
 
 
-exports.handler = async function http(req) {
+exports.handler = async function http(request) {
   return {
-    headers: {'content-type': 'text/html; charset=utf8'},
-    body: `<!doctype html>
-<html>
-<body>
-<h1> Web Sockets </h1>
-<main> Loading ... </main>
-<input id=message type=text placeholder="Enter message" autofocus>
-<script>
-window.WS_URL = '${getURL()}'
-</script>
-<script type=module src="/_static/index.mjs"></script>
-<script type=module src=${static('index.mjs')}></script>
-</body>
-</html>`
+    status: 201,
+    type: 'text/html; charset=utf8',
+    body: `
+      <!doctype html>
+      <html>
+        <body>hello world</body>
+      </html>
+    `
   };
 };
