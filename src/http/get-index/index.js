0425096ec3d849636fc37bc75a268cd3f3181482
Paul <pchinjr@gmail.com>
2019-07-08 20:45:43 -0400

    changes

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,3 +1,2 @@
-let arc = require('@architect/functions');
+const arc = require('@architect/functions');
 exports.handler = arc.proxy.public({spa: true});
-console.log('get index handler')
--- src/ws/ws-connect/index.js
+++ src/ws/ws-connect/index.js
@@ -4,7 +4,6 @@
  * - non 200 response will disconnect the client socket
  */
 exports.handler = async function ws(event) {
-  console.log(event.requestContext.apiId)
   console.log('ws-connect called with', event)
   return {statusCode: 200}
 }
